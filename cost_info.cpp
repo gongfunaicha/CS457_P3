f9e4dce56847a374faf26cdd6c3d47f664b709aa
Chen Wang <gongfunaicha@gmail.com>
2016-12-04 21:50:11 -0700

    Added cost info class

---
+++ cost_info.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Chen Wang on 12/3/16.
+//
+
+#include "cost_info.h"
+
+cost_info::cost_info(int source, int destination, int numcost)
+{
+    src = source;
+    dest = destination;
+    cost = numcost;
+}
---
+++ cost_info.h
@@ -0,0 +1,18 @@
+//
+// Created by Chen Wang on 12/3/16.
+//
+
+#ifndef CS457_P3_COST_INFO_H
+#define CS457_P3_COST_INFO_H
+
+
+class cost_info {
+public:
+    int src;
+    int dest;
+    int cost;
+    cost_info(int source, int destination, int numcost);
+};
+
+
+#endif //CS457_P3_COST_INFO_H
